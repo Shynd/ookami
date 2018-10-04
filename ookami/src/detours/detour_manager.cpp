d9e14a71f9b992159c4083748e3e4dd73fe3c273
Shynd <shynd@protonmail.com>
2018-10-04 15:58:23 +0200

    SMSG_WARDEN_DATA detour

--- ookami/src/detours/detour_manager.cpp
+++ ookami/src/detours/detour_manager.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <map>
 
+#include <ookami/functions/lua.hpp>
+
 std::map<std::string, MemoryOperation*> dt_mem{};
 
 int __fastcall SignalEventDetour(int a1)
@@ -24,8 +26,28 @@ int __fastcall SignalEventDetour(int a1)
   return result;
 }
 
+int __fastcall WardenDataDetour(int a1, int a2, int a3, int a4)
+{
+  char buffer[256];
+  snprintf(buffer, sizeof(buffer),
+           "[SMSG_WARDEN_DATA] [unk0: %d] [unk1: %d] [unk2: %d] [unk3: %d]", a1,
+           a2, a3, a4);
+  Lua::PrintChat(buffer);
+
+  // ---- return to original function ----
+  auto detour = dt_mem["SMSG_WARDEN_DATA"];
+  detour->Restore();
+  auto result =
+    ((int(__fastcall*)(int, int, int, int))detour->target)(a1, a2, a3, a4);
+  detour->Apply();
+  return result;
+}
+
 void DetourManager::Initialize()
 {
   dt_mem["FrameScript__SignalEvent"] =
     new Detour(FRAMESCRIPT__SIGNALEVENT, (int)SignalEventDetour);
+
+  dt_mem["SMSG_WARDEN_DATA"] =
+    new Detour(SMSG_WARDEN_DATA, (int)WardenDataDetour);
 }
